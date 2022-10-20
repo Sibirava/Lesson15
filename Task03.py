1a22f6462bf00f60d91a80ff03b32557c20cceff
sibirava <sibirava@gmail.com>
2022-10-20 21:54:34 +0300

    refactoring by string module, all and any functions

---
+++ Task03.py
@@ -0,0 +1,9 @@
+dictionary(key: value)
+#key должен быть уникален и immutable.
+# Листы не могут быть, сами словари . а остальные типы можно: string, tuple, number
+
+students = {'Nikita': 6, 'Danils': 5}
+# вытаскивать будет ключ
+
+students.get ---> None
+set (списки)
